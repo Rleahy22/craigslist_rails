079179dbc4aee2d4ae2d03d87feeeb3b1dec9d00
dbc-apprentice <apprentice@devbootcamp.com>
2013-07-15 17:32:07 -0500

    repush category

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,11 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @posts = Category.find(params[:id]).posts
+  end
+  
+end
